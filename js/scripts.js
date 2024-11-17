1f61e363916ee8a200ffb8dfe1725027cd288e33
Your Name <you@example.com>
2024-11-17 16:10:24 +0600

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+// This function is to show hello world
+
+
+/*\
+ 1, dfdff
+ 3 dfd
+4. multiline comments
+
+*/
+//dfdfd
+
+
+//  var overvalue = "24 octobor";
+
+//  if (overvalue  ==  "24 octobor"){
+
+//     console.log ("Happy birthday to you dhongi Joya Treat da.Treat na dila bondhu kotha koi na ");
+
+//  }
+
+//  else {
+//      console.log (" sorry ");
+//        
+
+var students = [
+
+
+    
+    {
+        "name": "bithe",
+
+        "SerialNo": "1",
+        "TanNo": "343dfd"
+    },
+    {
+        "name": "marufa",
+
+        "SerialNo": "2",
+        "TanNo": "343dff"
+    },
+    console.log( courses[1]['categories'][0] );
+
+]
+   
